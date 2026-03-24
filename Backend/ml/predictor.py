7c9e1933eff8e784d19328333b5b537f441ec419
Darshan Patel <pateldarshanp02@gmail.com>
2026-03-24 21:00:09 +0530

    fix model loading issue

--- Backend/ml/predictor.py
+++ Backend/ml/predictor.py
@@ -2,9 +2,16 @@ import joblib
 import os
 
 model_path = os.path.join(os.path.dirname(__file__), '../doctor/trained_model.joblib')
-model = joblib.load(model_path)
+
+model = None
 
 def predict(symptom_vector):
+    global model
+
+    if model is None:
+        model = joblib.load(model_path)
+
     prediction = model.predict([symptom_vector])[0]
     confidence = round(model.predict_proba([symptom_vector]).max() * 100, 2)
+
     return prediction, confidence
